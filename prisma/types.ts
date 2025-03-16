03bf865346b3c6dddf7337c572040f32f6598585
Isaias Lourenço <isaias.lourenco2020@outlook.com>
2025-03-16 13:08:53 -0300

    Adicionada ordenação, linguagem e gênero, além da listagem da view

---
+++ prisma/types.ts
@@ -0,0 +1,9 @@
+export type TitleLanguageGenre = {
+    id: number;
+    title: string;
+    release_date: Date | null;
+    oscar_count: number | null;
+    language_name: string;
+    genre_name: string;
+  };
+  
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,41 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
+import { TitleLanguageGenre } from "../prisma/types";
 
 const port = 3000;
 const app = express();
-const prisma = new PrismaClient
+const prisma = new PrismaClient();
 
 app.get("/", (req, res) => {
     res.send("Home Page");
 });
 
-app.get("/movies", async (req, res) => {
-    const movies = await prisma.movie.findMany();
+app.get("/movies", async (_, res) => {
+    const movies = await prisma.movie.findMany({
+        orderBy: {
+            id: "asc",
+        },
+        include: {
+            genres: true,
+            languages:true
+        }
+    });
     res.json(movies)
+});
+
+app.get("/movies-view", async (_, res) => {
+    try {
+        // Usando a tag Prisma.sql para a consulta SQL
+        const moviesView: TitleLanguageGenre[] = await prisma.$queryRaw<
+            TitleLanguageGenre[]
+        >(Prisma.sql`SELECT * FROM title_language_genre ORDER BY id ASC`);
+
+        // Respondendo com os dados como JSON
+        res.json(moviesView);
+    } catch (error) {
+        console.error("Erro ao consultar a view:", error);
+        res.status(500).json({ error: "Erro interno do servidor" });
+    }
 });
 
 app.listen(port, () => {
