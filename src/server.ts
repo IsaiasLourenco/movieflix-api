978087a1e2e66a8e15eaa844908a3e3e6b0b5166
Isaias Lourenço <isaias.lourenco2020@outlook.com>
2025-03-16 17:48:39 -0300

    Adicionada mensagem de erro na criação do filme

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
+// import { Request, Response, Application } from "express";
 import { PrismaClient, Prisma } from "@prisma/client";
 import { TitleLanguageGenre } from "../prisma/types";
 
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Home Page");
@@ -17,7 +20,7 @@ app.get("/movies", async (_, res) => {
         },
         include: {
             genres: true,
-            languages:true
+            languages: true
         }
     });
     res.json(movies)
@@ -37,6 +40,43 @@ app.get("/movies-view", async (_, res) => {
         res.status(500).json({ error: "Erro interno do servidor" });
     }
 });
+
+app.post("/movies", async (req, res) => {
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+        try {
+        await prisma.movie.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date)
+            }
+        });
+    } catch(error) {
+        return res.status(500).send({ message: "Falha ao cadastrar filme: ", error })
+    }
+        
+    res.status(201).send();
+    
+});
+
+app.put("/movies/:id", async (req, res) => {
+    //pegar o id do registro que vai ser atualziado
+    const id = Number(req.params.id);
+    //pegar os dados do filme que vai ser atualizado
+    const movie = await prisma.movie.update({
+        where: {
+            id
+        },
+        data: {
+            release_date: new Date (req.body.release_date)
+        }
+    })
+    // retornar o status correto informando que o filme foi atualizado
+    res.status(200).send();
+})
 
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
